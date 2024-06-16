b0c0f88aab0b03feaea89ddfefdcd2424c93c2d7
spidermp <markap.dev@gmail.com>
2024-06-16 09:05:31 -0500

    Removed a console.log

--- src/main.js
+++ src/main.js
@@ -4,7 +4,4 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
 
-console.log('API Base URL:', process.env.VUE_APP_API_BASE_URL);
-
-
 createApp(App).use(router).mount('#app')
