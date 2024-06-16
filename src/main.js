1a51c9a32d78d6c2d9b66572063d1cf9dfb789ae
spidermp <markap.dev@gmail.com>
2024-06-16 06:39:18 -0500

    Add console log to verify environment variable

--- src/main.js
+++ src/main.js
@@ -4,4 +4,7 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
 
+console.log('API Base URL:', process.env.VUE_APP_API_BASE_URL);
+
+
 createApp(App).use(router).mount('#app')
